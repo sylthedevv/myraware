b4b530d1ba2b10387dc0fb406461e350443226f7
sylv <hai@sylv.lol>
2025-07-13 15:12:51 -0400

    why is the gun umm

--- s/st/config.js
+++ s/st/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/st/go/',
-    bare:'https://amgis-erab.yourmom.eu.org/',
+    bare: 'https://6qjvmz.152.53.38.100.nip.io/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/s/st/handler.js',
